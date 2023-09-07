b7c6c5d6cb2196be9e329891da2766b2fd8146fc
Tanvir Ahmed <tanvir007v@gmail.com>
2023-09-07 13:03:45 -0500

    play with useState Hooks in React

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -1,14 +1,31 @@
-import React from "react";
+import { useState } from "react";
 
 const Content = () => {
+  const [name, setName] = useState("tanvir");
+  const [count, setCount] = useState(0);
+
   const handleNameChnage = () => {
     const names = ["tanvir", "ahmed", "badhon"];
-    const int = Math.floor(Math.random() * 5);
-    return names[int];
+    const int = Math.floor(Math.random() * 3);
+    setName(names[int]);
   };
+
+  const handleClick = () => {
+    setCount(count + 1);
+    setCount(count + 1);
+    console.log(count);
+  };
+
+  const handleClick2 = (name) => {
+    console.log(count);
+  };
+
   return (
     <main>
-      <p>Hello {handleNameChnage()}!</p>
+      <p onDoubleClick={handleClick}>Hello {name}!</p>
+      <button onClick={handleNameChnage}>Change Name</button>
+      <button onClick={handleClick}> Click It</button>
+      <button onClick={handleClick2}>Click It</button>
     </main>
   );
 };
